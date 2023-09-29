df9cde620a199dfca7abe20d427070cf4ba6e5fc
Minjik06 <minjik525@deltalearns.ca>
2023-09-29 13:29:43 -0700

    sf

--- task2.py
+++ task2.py
@@ -6,9 +6,10 @@ Create a class method that calculates the amount of compound interest for a give
 
 Extension: accept time given in different measurements, but convert them to years for use in your class template.
 """
-
+import math
 
 class Calc:
+    
     principal = 0
     rate = 0
     nPeriods = 0
@@ -27,6 +28,33 @@ class Calc:
     def amount(self,t):
         return round(self.principal*((1+(self.rate/self.nPeriods))**(self.nPeriods*t)),2)
 
+    def setYearAmount(self,t):
+        if t!=str(t):
+            self.amount(t)
+        else:
+            t1 = [int(i) for i in t.split() if i.isdigit()]
+            if 'days' in t:
+                t2=t1[0]/365
+                self.amount(t2)
+            elif 'months' in t:
+                t2=t1[0]/12
+                self.amount(t2)
+            elif 'weeks' in t:
+                t2=t1[0]/52
+                self.amount(t2)
+
+    def setYearInterest(self,t):
+        t1 = [int(i) for i in t.split() if i.isdigit()]
+        if 'days' in t:
+            t2=t1[0]/365
+            self.interest(t2)
+        elif 'months' in t:
+            t2=t1[0]/12
+            self.interest(t2)
+        elif 'weeks' in t:
+            t2=t1[0]/52
+            self.interest(t2)
+        
 
 
 
@@ -37,3 +65,10 @@ assert a.amount(5) == 1218.99
 b = Calc(P=5000,r=5.25,n=12)
 assert b.interest(10) == 3442.62
 
+c = Calc(P=5000,r=5.25,n=12)
+assert c.setYearAmount(10) == 5007.18
+
+
+"""return round(self.principal*((1+(self.rate/self.nPeriods))**(self.nPeriods*t)),2)"""
+
+print(c.setYearInterest('10 days'))
